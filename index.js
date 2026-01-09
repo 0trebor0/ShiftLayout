c05f202fc89a30ae0b2f281cb80aa8f2fd47d6be
robert <webdevme@outlook.com>
2026-01-09 18:35:27 +0000

    Refactor index.js to use ShiftLayout for conversion

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const { htmlToAndroidXML, convertWithResources } = require('./lib.js');
-const fs = require('fs');
+const ShiftLayout = require('./lib.js');
+const sculptor = new ShiftLayout({ useConstraint: true });
+
+const html = `
+    <div id="login_card" style="background-color: white; padding: 20px; width: 100%;top:0;">
+    <h1 style="color: black; font-size: 24px;">Register</h1>
+    <input type="text" id="inputusername" placeholder="Username" style="margin: 10px;padding: 10px;background-color:yellow;">
+    <input type="text" id="inputemail" placeholder="Email" style="margin: 10px;padding: 10px; background-color:yellow;">
+    <input type="text" id="inputpassword" placeholder="Password" style="margin: 10px; padding: 10px; background-color:yellow;">
+    <button style="background-color: blue; color: white;">Submit</button>
+</div>
+`;
+
+console.log(sculptor.convert(html));
